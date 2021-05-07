e2b605704375a83cf114acf16a9776bc27ae623e
Tim Carry <tim@pixelastic.com>
2021-05-07 23:49:42 +0200

    Pull reddit data

--- norska.config.js
+++ norska.config.js
@@ -1,4 +1,4 @@
-const theme = require('norska-theme-search');
+const theme = require('norska-theme-search-infinite');
 module.exports = {
   theme,
 };
--- src/_data/config.js
+++ src/_data/config.js
@@ -2,5 +2,6 @@ module.exports = {
   algolia: {
     appId: 'O3F8QXYK6R',
     apiKey: '70312942d736090b28f61534e4faeea3',
+    indexName: 'gamemaster_craft',
   },
 };
