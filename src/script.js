0ca94cd4583df3fe1886407489b993e907ac315a
Tim Carry <tim@pixelastic.com>
2021-05-08 09:17:02 +0200

    Add search

--- src/script.js
+++ src/script.js
@@ -1,4 +1,16 @@
 const theme = require('norska/theme');
+const lazyloadAttributes = require('norska/frontend/lazyload/attributes');
+
 (async () => {
-  await theme.init();
+  await theme.init({
+    placeholder: 'Search for dungeon tiles, trees, houses, anything!',
+    hitName: 'project',
+    transforms: {
+      preview(item) {
+        const previewUrl = item.picture.preview;
+        const options = { width: 600, placeholder: { width: 200 } };
+        return lazyloadAttributes(previewUrl, options);
+      },
+    },
+  });
 })();
